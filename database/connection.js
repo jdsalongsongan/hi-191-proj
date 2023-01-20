ad3bced68c70c6737c4fc5f3d92ca034b5654637
jdsalongsongan <jdsalongsongan@up.edu.ph>
2023-01-20 16:40:08 +0800

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -3,14 +3,8 @@ require('dotenv').config()
 
 const db = knex({
     client: 'pg',
-    connection: {
-        host: process.env.HOST,
-        user: process.env.USER,
-        port: process.env.DB_PORT,
-        password: process.env.PASSWORD,
-        database: process.env.DB
-    } 
-    // connection: process.env.DATABASE
+
+    connection: process.env.DATABASE
 })
 
 module.exports = db;
